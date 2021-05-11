109e4a06aa98cebdb7089fa4b67ea17f3aa6d7c0
Kai Aoyama <sauna.kaikai@gmail.com>
2021-05-12 02:19:39 +0900

    delegate-practice

--- Keyboard1/Keyboard1/ViewController.swift
+++ Keyboard1/Keyboard1/ViewController.swift
@@ -7,13 +7,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate { //UITextFieldDelegateを記述することで、UITextFieldが持っている、delegateメソッドを呼べるようになる。delegateメソッドの仕様書のことを、プロトコルという。
 
+    @IBOutlet weak var logoImageView: UIImageView!
+    
+    
+    @IBOutlet weak var userNameTextField: UITextField!
+    
+    
+    @IBOutlet weak var passWordTextField: UITextField!
+    
+    
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    @IBOutlet weak var passWordLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        userNameTextField.delegate = self  //ここでの記述で、実際にメソッドを使用できるようにする。
+//        passWordTextField.delegate = self  //ここでのselfは、ViewControllerのこと。
     }
+    
+    
 
-
+    @IBAction func login(_ sender: Any) {
+        
+        logoImageView.image = UIImage(named: "loginOK")
+        
+        userNameLabel.text = userNameTextField.text
+        passWordLabel.text = passWordTextField.text
+        
+    }
+    
+    //画面をタップしたら、キーボードが閉じる
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    //returnを押したら、キーボードが閉じる
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {  //textFieldのreturnが押された時
+        textField.resignFirstResponder()    //textfieldのキーボードを閉じる処理
+    }
+    
 }
+
 
