986bbbd1177beda7fc9b0802ae57090fe71016ce
Kai Aoyama <sauna.kaikai@gmail.com>
2021-05-12 00:57:16 +0900

    storyboard-setting

---
+++ Keyboard1/Keyboard1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Keyboard1
+//
+//  Created by ヘパリン類似物質 on 2021/05/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
