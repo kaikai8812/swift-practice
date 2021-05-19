c54289d9784fd7f07cfd51d9875e7f68468b409b
Kai Aoyama <sauna.kaikai@gmail.com>
2021-05-19 21:23:44 +0900

    お題データの受信、回答データの送信記述、完了

--- OdaiApp/OdaiApp/Controller/ViewController.swift
+++ OdaiApp/OdaiApp/Controller/ViewController.swift
@@ -10,19 +10,81 @@ import Firebase
 import FirebaseFirestore
 
 class ViewController: UIViewController {
+    
+    //お題データを受信するデータベースの場所を設定
+    let db1 = Firestore.firestore().collection("Odai").document("6fqTXCag3aCbkQM1RwIZ")
+    
+    //回答データを送信するデータベースの場所を設定するための変数
+    let db2 = Firestore.firestore()
+    
+    var userName = String()
+    
+    
+    @IBOutlet weak var textview: UITextView!
+    
+    
+    @IBOutlet weak var odaiLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        if UserDefaults.standard.object(forKey: "userName") != nil{
+            userName = UserDefaults.standard.object(forKey: "userName") as! String
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        navigationController?.isNavigationBarHidden = false
+        navigationController?.isNavigationBarHidden = true
+        loadQuestiondata()
+        
     }
     
-    let db1 = Firestore.firestore().collection("Odai").document("6fqTXCag3aCbkQM1RwIZ")
+    func loadQuestiondata(){
+        
+        //指定したドキュメントから、実際にデータを引っ張ってくる処理
+        
+        db1.getDocument { snapShot, error in
+            
+            if error != nil {
+                print(error.debugDescription)
+                return
+            }
+            
+           let data = snapShot?.data()
+            
+            self.odaiLabel.text = data!["odaiText"] as! String
+        }
+    }
+    
+    
+    //FireStoreに、回答のデータを送信する
+    @IBAction func send(_ sender: Any) {
+        
+        //setDataの方は、コメントアウトしています。
+//        db2.collection("Answers").document().setData(["answer" : textview.text as Any, "userName": userName as Any, "postDate":Date().timeIntervalSince1970 ])
+        
+        db2.collection("Answers").addDocument(data: ["answer" : textview.text as Any, "userName": userName as Any, "postDate":Date().timeIntervalSince1970 ]) { error in
+
+            if error != nil{
+                print(error.debugDescription)
+                return
+            }
+
+        }
+        
+        print("送りました")
+
+        
+    }
+    
+    
+    
+    
+  
     
     
     
