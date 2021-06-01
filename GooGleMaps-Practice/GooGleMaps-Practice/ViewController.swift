10c93304f832e180b18b1c84fc8caa7aa7b934cd
Kai Aoyama <sauna.kaikai@gmail.com>
2021-06-01 21:13:24 +0900

    googleMaps準備

---
+++ GooGleMaps-Practice/GooGleMaps-Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GooGleMaps-Practice
+//
+//  Created by ヘパリン類似物質 on 2021/06/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GooGleMaps-Practice/Pods/Target Support Files/Pods-GooGleMaps-Practice/Pods-GooGleMaps-Practice-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GooGleMaps_Practice : NSObject
+@end
+@implementation PodsDummy_Pods_GooGleMaps_Practice
+@end
