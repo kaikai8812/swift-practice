873277b77424b88e892bba37e0d334fa35bf5ffd
Kai Aoyama <sauna.kaikai@gmail.com>
2021-06-02 16:08:14 +0900

    googleMapsAPI,現在位置の取得=>地図に反映

--- GooGleMaps-Practice/GooGleMaps-Practice/ViewController.swift
+++ GooGleMaps-Practice/GooGleMaps-Practice/ViewController.swift
@@ -7,29 +7,61 @@
 
 import UIKit
 import GoogleMaps
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,CLLocationManagerDelegate {
+    
+    let manager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        manager.delegate = self
+        //アプリの使用中に、ユーザーに位置情報サービスの使用許可を求める記述
+        manager.requestWhenInUseAuthorization()
+        //ユーザーの現在地を報告する更新の生成  ここで、デバイスの現在位置をmanagerに設定している？
+        manager.startUpdatingLocation()
         
         GMSServices.provideAPIKey("AIzaSyBW1Tc5IvfPjm_3x-v7CRIQ3z2MLZNqYhM")
         
         //ここで、マップ表示時の位置を決めている
         let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
-                let mapView = GMSMapView.map(withFrame: self.view.frame, camera: camera)
-                self.view.addSubview(mapView)
+        let mapView = GMSMapView.map(withFrame: self.view.frame, camera: camera)
+        self.view.addSubview(mapView)
         
         //ここで、マップにつけるマーカーの位置を決めている。
         let marker = GMSMarker()
-                marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
         //マーカーのタイトル等を、決めている
-                marker.title = "Sydney"
-                marker.snippet = "Australia"
-                marker.map = mapView
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
         
     }
-
-
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        //locationsに、位置情報が入る。locationに何も入らなかったら、処理をreturnする。
+        guard let location  = locations.first else {
+            return
+        }
+        //coordinate ==　座標
+        let coordinate = location.coordinate
+        
+        //取得した座標から、
+        let camera = GMSCameraPosition.camera(withLatitude: coordinate.latitude, longitude: coordinate.longitude, zoom: 6.0)
+        let mapView = GMSMapView.map(withFrame: self.view.frame, camera: camera)
+        self.view.addSubview(mapView)
+        
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: coordinate.latitude, longitude: coordinate.longitude)
+        //マーカーのタイトル等を、決めている
+        marker.title = "現在地です"
+        marker.snippet = "現在地だよ"
+        marker.map = mapView
+    }
+    
+    
 }
 
+
+//やりたいこと =>
