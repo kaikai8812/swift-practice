a61a2f5d4f9919271ecbb10ccda31138d0d4ea33
Kai Aoyama <sauna.kaikai@gmail.com>
2021-06-04 18:44:33 +0900

    ハッシュタグ別データ保存

--- Qiita-HashTag/Qiita-HashTag/ViewController.swift
+++ Qiita-HashTag/Qiita-HashTag/ViewController.swift
@@ -16,16 +16,25 @@ class ViewController: UIViewController {
     //投稿するボタンに紐づいています。
     @IBAction func send(_ sender: Any) {
         //入力された文字列を引数に、下記に記述したメソッドを呼んでいます。
-        sendDB(text: textField.text!)
-    }
-    
-    //入力されたデータを、fireStoreに保存するための記述です。
-    func sendDB(text:String) {
-        //fireStoreに保存する処理
-        Firestore.firestore().collection("collection").document().setData(["text" : text])
+        Firestore.firestore().collection("collection").document().setData(["text" : textField.text!])
+        
+        //追加部分 ==================================================================
+        let hashTagText = textField.text as NSString?
+        
+        do{
+            let regex = try NSRegularExpression(pattern: "#\\S+", options: [])
+            //見つけたハッシュタグを、for文で回す。
+            for match in regex.matches(in: hashTagText! as String, options: [], range: NSRange(location: 0, length: hashTagText!.length)) {
+                
+                //見つかったハッシュタグを引数に入れデータベースに保存
+                Firestore.firestore().collection(hashTagText!.substring(with: match.range)).document().setData(["text" : self.textField.text!])
+            }
+        }catch{
+        }
+        //追加部分 ==================================================================
+        
         //データを保存したら、画面遷移を行います。
         let tableVC = storyboard?.instantiateViewController(identifier: "tableVC") as! TableViewController
         navigationController?.pushViewController(tableVC, animated: true)
     }
 }
-
